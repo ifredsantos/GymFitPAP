8dc9a7cf85e85bd4b2515aacac8aafcb4f1c56f2
Frederico Santos <fsraen@gmail.com>
2024-09-03 13:11:52 +0100

    Reorganização.

--- App Web/_Documentacao/BaseDados/SQLPagamentos.sql
+++
@@ -1,7 +0,0 @@
--- INSERT INTO easypay_references 
--- (ep_cin, ep_status, ep_message, ep_entity, ep_reference, t_key, o_obs, o_mobile, o_email, ep_value)
-
--- CREATE TABLE esyp_ref (
---     cod_ref_tansacao INT NOT NULL AUTO_INCREMENT,
---     ep_cin 
--- )
